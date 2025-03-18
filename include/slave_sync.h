e6ee9aacd3a474abee1708ca62892d90378b908f
cai luoshan <cailuoshan@node005.bosccluster.com>
2025-03-18 14:48:45 +0800

    Diffsync: for qemu as slave, recv data through virtio-serial and socket connect

--- include/slave_sync.h
+++ include/slave_sync.h
@@ -4,82 +4,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#include <sys/mman.h>
 #include <unistd.h>
-#include <semaphore.h>
-#include <signal.h>
 
-#define SHM_NAME "/my_shared_memory"
-#define SEM_NAME "/my_semaphore"
+#define VIRTIO_PATH "/dev/vport1p1"
 
 struct SyncInfo {
-    int    shm_fd;
-    void  *shm_ptr;
-    sem_t *sem;
-    pid_t  host_pid;
+    int  virtio_fd;
 };
 typedef struct SyncInfo SyncInfo;
 
-SyncInfo init_sync_env(size_t shm_size) {
+SyncInfo init_sync_env() {
     SyncInfo sync_info;
-
-    // 读取 Host 的 PID
-    FILE *fp = fopen("/nfs/home/cailuoshan/AISim/Diffsync_Skip/host.pid", "r");
-    if (fscanf(fp, "%d", &(sync_info.host_pid)) != 1) {
-        perror("Failed to read PID");
-        fclose(fp);
-        return sync_info;
+    // 打开 virtio 设备文件，从而连接 qemu 创建的 socket 端口
+    sync_info.virtio_fd = open(VIRTIO_PATH, O_RDWR);
+    if (sync_info.virtio_fd < 0) {
+        perror("Can't open VirtIO device");
     }
-    fclose(fp);
-    
-    // 打开共享内存
-    sync_info.shm_fd = shm_open(SHM_NAME, O_RDONLY, 0666);
-    if (sync_info.shm_fd == -1) {
-        perror("无法打开共享内存");
-        return sync_info;
-    }
-
-    // 映射共享内存
-    sync_info.shm_ptr = mmap(0, shm_size, PROT_READ, MAP_SHARED, sync_info.shm_fd, 0);
-    if (sync_info.shm_ptr == MAP_FAILED) {
-        perror("mmap 失败");
-        return sync_info;
-    }
-
-    // 打开信号量
-    sync_info.sem = sem_open(SEM_NAME, 0);
-    if (sync_info.sem == SEM_FAILED) {
-        perror("信号量打开失败");
-        return sync_info;
-    }
-
     return sync_info;
 }
 
-void close_sync_env(SyncInfo sync_info, size_t shm_size) {
-    // 释放资源
-    munmap(sync_info.shm_ptr, shm_size);
-    close(sync_info.shm_fd);
-    sem_close(sync_info.sem);
-    sem_unlink(SEM_NAME);
-    shm_unlink(SHM_NAME);
+void send_req_to_host(SyncInfo sync_info) {
+    char msg[] = "DATA_REQ";
+    write(sync_info.virtio_fd, msg, strlen(msg));
+    printf("\nQEMU: Send req to wake up host.\n");
 }
 
-void SlaveSync(void *dest_data, size_t datalen) {
-    SyncInfo sync_info = init_sync_env(datalen);
-    
-    // 发送 SIGUSR1 信号唤醒主进程
-    printf("独立进程: 发送 SIGUSR1 信号给 %d\n", sync_info.host_pid);
-    kill(sync_info.host_pid, SIGUSR1);
+void receive_data(SyncInfo sync_info, void *dest_data, size_t data_len) {
+    // 单次传输大小受限于 Linux PIPE_BUF(4096 Bytes)，通过循环 read 接收所有数据
+    size_t total_received = 0;
+    while (total_received < data_len) {
+        ssize_t received = read(sync_info.virtio_fd, dest_data + total_received, data_len - total_received);
+        if (received < 0) {
+            perror("QEMU: Fail to read data from VirtIO");
+            return;
+        } else if (received == 0) {
+            printf("QEMU: Disconnected from Host.\n");
+            return;
+        }
+        total_received += received;
+    }
+    printf("QEMU: Finish receive %zu bytes data.\n", total_received);
+}
 
-    // 等待信号量（确保主进程已经写入数据）
-    sem_wait(sync_info.sem);
+void close_sync_env(SyncInfo sync_info) {
+    close(sync_info.virtio_fd);
+}
 
-    // 将数据拷贝至目标地址
-    memcpy(dest_data, sync_info.shm_ptr, datalen);
-
-    close_sync_env(sync_info, datalen);
-    sleep(1);
+void SlaveSync(void *dest_data, size_t data_len) {
+    SyncInfo sync_info = init_sync_env();
+    send_req_to_host(sync_info);
+    receive_data(sync_info, dest_data, data_len);
+    close_sync_env(sync_info);
 }
 
 #endif
